137285b7e32b815079a3bd786721b5099737b7b3
Ye Hao <uuau99999@gmail.com>
2017-09-29 14:33:35 +0800

    fix module export in RNIconBadge.js

--- RNIconBadge.js
+++ RNIconBadge.js
@@ -2,10 +2,8 @@ var RNIconBadge = require('react-native').NativeModules.RNIconBadge
 var PushNotificationIOS = require('react-native').PushNotificationIOS
 var Platform = require('react-native').Platform
 
-var iosBadgeModule = {}
-iosBadgeModule.setIconBadge = PushNotificationIOS.setApplicationIconBadgeNumber
-iosBadgeModule.getBadgeNumber =
-  PushNotificationIOS.getApplicationIconBadgeNumber
-iosBadgeModule.clearBadge = PushNotificationIOS.setApplicationIconBadgeNumber(0)
-
-module.exports = Platform.OS === 'ios' ? iosBadgeModule : RNIconBadge
+module.exports = Platform.OS === 'ios' ? {
+  setIconBadge: PushNotificationIOS.setApplicationIconBadgeNumber,
+  getBadgeNumber: PushNotificationIOS.getApplicationIconBadgeNumber,
+  clearBadge: PushNotificationIOS.setApplicationIconBadgeNumber(0),
+} : RNIconBadge
